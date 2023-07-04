4c868c447cd086e1edcd59633fa5034f7baf6efa
vano31 <vanosike31@gmail.com>
2023-07-04 14:09:30 -0400

    Config npm, webpack, and firebase

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9bHwSXIAmYRFUzUGjrL0ScA011zGM1d0",
+    authDomain: "online-game-backend.firebaseapp.com",
+    projectId: "online-game-backend",
+    storageBucket: "online-game-backend.appspot.com",
+    messagingSenderId: "562748717352",
+    appId: "1:562748717352:web:2ec3f6dbe3added85bb81d"
+  };
+
+
+
